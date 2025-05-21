0fff865236b3d08c5600b84e36b712cb12e47418
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-05-21 14:39:19 +0900

    15 minutes

--- db/data/seeds/002-tasks.js
+++ db/data/seeds/002-tasks.js
@@ -1,28 +1,40 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 
 const taskData = [];
 
-const taskList = ['breakfast', 'lunch', 'dinner', 'running', 'study', 'meeting', 'hospital', 'cooking', 'game', 'training', 'skiing',]
+const taskList = [
+  'breakfast',
+  'lunch',
+  'dinner',
+  'running',
+  'study',
+  'meeting',
+  'hospital',
+  'cooking',
+  'game',
+  'training',
+  'skiing',
+];
 
 function createDummy(n) {
-  for(let i = 0; i < n; i ++) {
-    const user_id = Math.floor(Math.random() * 10 +1);
+  for (let i = 0; i < n; i++) {
+    const user_id = Math.floor(Math.random() * 10 + 1);
 
     const year = 2025;
     const month = 5;
 
-    const date = Math.floor(Math.random() * 31 +1);
+    const date = Math.floor(Math.random() * 31 + 1);
 
-    const hour = Math.floor(Math.random() * 24 +1);
+    const hour = Math.floor(Math.random() * 24 + 1);
 
-    const minute = Math.floor(Math.random() * 59 +1);
+    const minute = Math.floor(Math.floor(Math.random() * 59 + 1) / 15) * 15;
 
     const task = taskList[Math.floor(Math.random() * taskList.length)];
 
-    taskData.push({user_id, year, month, date, hour, minute, task})
+    taskData.push({ user_id, year, month, date, hour, minute, task });
   }
 }
 
@@ -30,6 +42,6 @@ createDummy(500);
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('tasks').del()
+  await knex('tasks').del();
   await knex('tasks').insert(taskData);
 };
