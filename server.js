3043da07c7984bb3953be453d82af0a98e641f6f
yuta_sato <yuta_sato@mail.toyota.co.jp>
2025-05-21 14:07:31 +0900

    修正#6

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const db = require('.');
 const app = express();
+require('dotenv').config();
+
 
 let PORT;
 if (process.env.NODE_ENV === 'development') {
